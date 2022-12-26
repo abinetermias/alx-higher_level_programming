77c7a2446f127e8252743ed021d0ca5d17d51088
abinetermias <abinet.gobena2022@gmail.com>
2022-12-25 23:28:34 -0800

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
-digit1 = 0
-digit2 = 1
-while digit1 <= 8:
-    while digit2 <= 9:
-        if digit1 != digit2:
-            print("{:d}".format(digit1), end='')
-                if digit1 != 8:
-                         print("{:d}, ".format(digit2), end='')
-                else:
-                         print("{:d}".format(digit2))
-          digit2 += 1
-      digit2 = digit1 + 1
-      digit1 = digit1 + 1
+
+for digit1 in range(0, 10):
+    for digit2 in range(digit1 + 1, 10):
+        if digit1 == 8 and digit2 == 9:
+            print("{}".format(digit1, digit2))
+        else:
+            print("{}".format(digit1, digit2), end=", ")
