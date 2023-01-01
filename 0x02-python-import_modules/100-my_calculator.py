42231595ef6bf52600c5bb92d44c35fef2b15a7a
abinetermias <abinet.gobena2022@gmail.com>
2023-01-01 13:07:50 -0800

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-import sys
-from calculator_1 import add, sub, mul, div
 
+from calculator_1 import add, sub, mul, div
+import sys
 if __name__ == '__main__':
     if len(sys.argv) - 1 != 3:
        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
