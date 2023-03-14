d51ad0bf132965581d86f2a10bb5d4952b75f45b
abinetermias <abinet.gobena2022@gmail.com>
2023-03-14 07:17:57 -0700

    9-logme.js

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,12 +1,3 @@
 #!/usr/bin/node
-// function that returns the reversed version of a list withouth reverse
-
-exports.esrever = function (list) {
-  const reversedList = [];
-  const last = list.length - 1;
-  for (let i = last; i >= 0; i--) {
-    reversedList.push(list[i]);
-  }
-  return (reversedList);
-};
-
+let count = 0;
+exports.logMe = function (item) { console.log(`${count++}: ${item}`); };
